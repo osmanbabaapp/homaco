3feacdc3fe87104c07641b2faccf675a24daf98e
Hamude shahin <76263045+hamudeshahin@users.noreply.github.com>
2023-01-21 12:49:54 +0300

    added 404 & 500 = trying to solve deployment error

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const Page404: FC = () => {
+  return (
+    <div>
+      <h2>404 Not Found</h2>
+    </div>
+  );
+};
+export default Page404;
---
+++ pages/500.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+
+const Page500: FC = () => {
+  return (
+    <div>
+      <h2>500 Error</h2>
+    </div>
+  );
+};
+export default Page500;
