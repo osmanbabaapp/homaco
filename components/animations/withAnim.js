bc10bb13e5293b7c10dd68084f967efdcb812208
Hamude shahin <76263045+hamudeshahin@users.noreply.github.com>
2023-02-01 23:34:46 +0300

    admin dashboard added 5 & commented animation hooks

--- components/animations/withAnim.js
+++ components/animations/withAnim.js
@@ -1,59 +1,58 @@
+// import React, { Component } from "react";
+// import PropTypes from "prop-types";
+// import styled, { keyframes } from "styled-components";
+// import  { slideInLeft, slideInRight, slideOutRight, slideInUp } from "react-animations";
 
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import styled, { keyframes } from "styled-components";
-import  { slideInLeft, slideInRight, slideOutRight, slideInUp } from "react-animations";
+// const AnimationWrapper = styled.div`
+//   animation: ${({ duration }) => duration}s ${({ animation }) => animation};
+//   // animation-iteration-count: infinite;
+// `;
 
-const AnimationWrapper = styled.div`
-  animation: ${({ duration }) => duration}s ${({ animation }) => animation};
-  // animation-iteration-count: infinite;
-`;
+// const withAnim = AnimComponent => {
+//   class WithAnimHoc extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         AsyncComponent: () => null,
+//         animation: this.props.animation,
+//         duration: this.props.duration,
+//       };
+//     }
 
-const withAnim = AnimComponent => {
-  class WithAnimHoc extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        AsyncComponent: () => null,
-        animation: this.props.animation,
-        duration: this.props.duration,
-      };
-    }
+//     async componentDidMount() {
+//       const myModule = await import(`react-animations/lib/${
+//         this.state.animation
+//       }`);
 
-    async componentDidMount() {
-      const myModule = await import(`react-animations/lib/${
-        this.state.animation
-      }`);
+//       const AsyncComponent = () => (
+//         <AnimationWrapper
+//           duration={this.state.duration}
+//           animation={keyframes`${myModule.default}`}
+//         >
+//           <AnimComponent {...this.props} />
+//         </AnimationWrapper>
+//       );
 
-      const AsyncComponent = () => (
-        <AnimationWrapper
-          duration={this.state.duration}
-          animation={keyframes`${myModule.default}`}
-        >
-          <AnimComponent {...this.props} />
-        </AnimationWrapper>
-      );
+//       this.setState({ AsyncComponent });
+//     }
 
-      this.setState({ AsyncComponent });
-    }
+//     render() {
+//       const { AsyncComponent } = this.state;
 
-    render() {
-      const { AsyncComponent } = this.state;
+//       return <AsyncComponent />;
+//     }
+//   }
 
-      return <AsyncComponent />;
-    }
-  }
+//   WithAnimHoc.defaultProps = {
+//     duration: "1.5"
+//   };
 
-  WithAnimHoc.defaultProps = {
-    duration: "1.5"
-  };
+//   WithAnimHoc.propTypes = {
+//     animation: PropTypes.string.isRequired,
+//     duration: PropTypes.string
+//   };
 
-  WithAnimHoc.propTypes = {
-    animation: PropTypes.string.isRequired,
-    duration: PropTypes.string
-  };
+//   return WithAnimHoc;
+// };
 
-  return WithAnimHoc;
-};
-
-export default withAnim;
+// export default withAnim;
