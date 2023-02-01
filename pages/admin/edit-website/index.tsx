07bb0d5d1fd5b29107dea3c69d5acdc69b7ca8c7
Hamude shahin <76263045+hamudeshahin@users.noreply.github.com>
2023-02-01 23:44:51 +0300

    admin dashboard added 7

--- pages/admin/edit-website/index.tsx
+++ pages/admin/edit-website/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import EditWebsitePageContent from "components/views/edit-website";
+// import EditWebsitePageContent from "components/views/edit-website";
 
 import axios from "axios";
 
@@ -18,14 +18,16 @@ export default function EditWebsitePage(props: any) {
       <Head>
         <title>Edit Website</title>
       </Head>
-      <EditWebsitePageContent data={props.data} products={props.products} locale={locale} />
+      {/* <EditWebsitePageContent data={props.data} products={props.products} locale={locale} /> */}
     </>
   );
 }
 
 EditWebsitePage.layout = "dashboard";
 
-const reqUrl = process.env.NEXT_PUBLIC_HOST! + process.env.NEXT_PUBLIC_ALL_HOME_PAGE_SETTINGS;
+const reqUrl =
+  process.env.NEXT_PUBLIC_HOST! +
+  process.env.NEXT_PUBLIC_ALL_HOME_PAGE_SETTINGS;
 
 export async function getServerSideProps(context: any) {
   const { locale } = context;
@@ -39,7 +41,10 @@ export async function getServerSideProps(context: any) {
   try {
     const { data: res } = await axios.get(
       reqUrl,
-      { httpsAgent: httpsAgent, headers: { websiteHostName: process.env.NEXT_PUBLIC_WEBSITE_HOST_NAME } } // for development only
+      {
+        httpsAgent: httpsAgent,
+        headers: { websiteHostName: process.env.NEXT_PUBLIC_WEBSITE_HOST_NAME },
+      } // for development only
     );
 
     if (res?.status === true) {
