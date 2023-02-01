a7ff39770403e74b493e2abb93df2a62b8b3a8d4
Muhammed Ubeyd <mhd.ubeyd@gmail.com>
2023-02-01 16:53:44 +0300

    update MachineSection1 style

--- components/sections/mach1-section.tsx
+++ components/sections/mach1-section.tsx
@@ -6,11 +6,12 @@ const MachineSection1: FC<{}> = () => {
   return (
     <div className="py-28 flex bg-white bg-[url('/imgs/mach-1.png')] bg-no-repeat bg-contain relative">
       <Container>
-        <h2 className="absolute z-10 text-2xl md:text-4xl   left-[20vw] top-[6vh] font-bold text-black  ">
+        <h2 className="absolute z-10 text-2xl md:text-4xl   left-[20vw] top-[4vh] font-bold text-black flex-wrap w-[200px] ">
           آلة طحن السمسم وانتاج الطحينة
         </h2>
         <motion.div
           style={{ position: "relative" }}
+          className="ml-[30vw] "
           whileInView={{
             opacity: 1,
             scale: 1,
@@ -24,13 +25,23 @@ const MachineSection1: FC<{}> = () => {
             scale: 0.5,
           }}
         >
-          <div className="md:w-[40%] sm:w-[70%] w-[80%] bg-white drop-shadow-lg p-5">
+          <div className="md:w-[40%] min-w-[50%] sm:w-[70%] w-[80%] bg-white drop-shadow-lg p-5 ">
+            {/* <div className="block"> */}
+            <section className="text-xl">لطحن السمسم و إنتاج أفضل أنواع الطحينية ,</section>
+
+            <section className="text-xl">
+              <section className="text-red-600 inline-block"> الستانلس </section>
+              تم تصميم هذه الآلة التي تعد من أهم الآلات التي قمنا بصناعتها , حيث استخدمنا في صناعتها أجود أنواع
+            </section>
+
             <p className="text-xl">
-              لطحن السمسم و إنتاج أفضل أنواع الطحينية , تم تصميم هذه الآلة التي تعد من أهم الآلات التي قمنا بصناعتها ,
-              حيث استخدمنا في صناعتها أجود أنواع الستانلس ستيل 304L بتحكم آلي بالكامل .. تمتاز هذه الآلة بطاقتها
-              الإنتاجية العالية لاحتوائها على أحدث أنواع أنظمة التحكم الالكتروني PLC برؤوس تنعيم للسمسم المطحون للحصول
-              على أجود أنواع الطحينة باللزوجة المثالية
+              ستيل 304L بتحكم آلي بالكامل .. تمتاز هذه الآلة بطاقتها الإنتاجية العالية لاحتوائها على أحدث أنواع أنظمة
             </p>
+
+            <p className="text-xl">
+              التحكم الالكتروني PLC برؤوس تنعيم للسمسم المطحون للحصول على أجود أنواع الطحينة باللزوجة المثالية
+            </p>
+            {/* </div> */}
           </div>
         </motion.div>
       </Container>
